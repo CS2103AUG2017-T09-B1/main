ed589f61751185e6772fcc7dc059e71527fbb805
LJH <jia_hao_95@hotmail.com>
2017-10-18 12:47:31 +0800

    Model: Implementation of Birthday parameters . Created a Birthday Class in person. . Added birthday examples to SampleDataUtil . Added comparator method in ReadOnlyPerson interface . Added Birthday parameter in Person class

---
+++ src/main/java/seedu/address/model/person/Birthday.java
@@ -0,0 +1,45 @@
+package seedu.address.model.person;
+
+/**
+ * Represents a Person's birthday in the address book.
+ * Guarantees: immutable; is always valid
+ */
+public class Birthday {
+
+    public final String value;
+
+    public Birthday(String birthday) {
+        this.value = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Birthday // instanceof handles nulls
+                && this.value.equals(((Birthday) other).value)); // state check
+           }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public String day;
+    public String month;
+    public String year;
+    public String age;
+
+    public String getAge() {
+        day =  this.value.substring(0,2);
+        month = this.value.substring(3,5);
+        year = this.value.substring(6,10);
+        age = year + month + day;
+
+        return age;
+    }
+}
--- src/main/java/seedu/address/model/person/ReadOnlyPerson.java
+++ src/main/java/seedu/address/model/person/ReadOnlyPerson.java
@@ -1,5 +1,7 @@
 package seedu.address.model.person;
 
+import java.util.Comparator;
+
 import java.util.Set;
 
 import javafx.beans.property.ObjectProperty;
@@ -20,8 +22,58 @@ public interface ReadOnlyPerson {
     Email getEmail();
     ObjectProperty<Address> addressProperty();
     Address getAddress();
+    ObjectProperty<Birthday> birthdayProperty();
+    Birthday getBirthday();
     ObjectProperty<UniqueTagList> tagProperty();
     Set<Tag> getTags();
+
+    Comparator<ReadOnlyPerson> COMPARE_BY_AGE = new Comparator<ReadOnlyPerson>() {
+        public int compare(ReadOnlyPerson firstNum, ReadOnlyPerson secondNum) {
+            String newFirstNum = getNewStringAgeFormat(firstNum);
+            String newSecondNum = getNewStringAgeFormat(secondNum);
+            if(newFirstNum.equals("") || newSecondNum.equals("")) {
+                return newSecondNum.compareTo(newFirstNum);
+            } else {
+                return newFirstNum.compareTo(newSecondNum);
+            }
+        }
+    };
+
+    Comparator<ReadOnlyPerson> COMPARE_BY_BIRTHDAY = new Comparator<ReadOnlyPerson>() {
+        public int compare(ReadOnlyPerson firstPerson, ReadOnlyPerson secondPerson) {
+            String newFirstNum = getNewStringBirthdayFormat(firstPerson);
+            String newSecondNum = getNewStringBirthdayFormat(secondPerson);
+            if(newFirstNum.equals("") || newSecondNum.equals("")) {
+                return newSecondNum.compareTo(newFirstNum);
+            } else {
+                return newFirstNum.compareTo(newSecondNum);
+            }
+        }
+    };
+
+    static String getNewStringAgeFormat(ReadOnlyPerson person) {
+        if(person.getBirthday().toString().equals("")) {
+            return "";
+        } else {
+            String numInString = person.getBirthday().toString();    // Converts birthday to String type
+            String dayForNum = numInString.substring(0, 2);        // Index of day in dd/mm/yyyy
+            String monthForNum = numInString.substring(3, 5);      // Index of month in dd/mm/yyyy
+            String yearForNum = numInString.substring(6, 10);      // Index of year in dd/mm/yyyy
+            return yearForNum + monthForNum + dayForNum;           // Return string format yyyymmdd
+        }
+    }
+
+    static String getNewStringBirthdayFormat(ReadOnlyPerson person) {
+        if(person.getBirthday().toString().equals("")) {
+            return "";
+        } else {
+            String numInString = person.getBirthday().toString();  // Converts birthday to String type
+            String dayForNum = numInString.substring(0, 2);        // Index of day in dd/mm/yyyy
+            String monthForNum = numInString.substring(3, 5);      // Index of month in dd/mm/yyyy
+            String yearForNum = numInString.substring(6, 10);      // Index of year in dd/mm/yyyy
+            return monthForNum + dayForNum + yearForNum;           // Return String format mmddyyy
+        }
+    }
 
     /**
      * Returns true if both have the same state. (interfaces cannot override .equals)
@@ -32,7 +84,8 @@ public interface ReadOnlyPerson {
                 && other.getName().equals(this.getName()) // state checks here onwards
                 && other.getPhone().equals(this.getPhone())
                 && other.getEmail().equals(this.getEmail())
-                && other.getAddress().equals(this.getAddress()));
+                && other.getAddress().equals(this.getAddress())
+                && other.getBirthday().equals(this.getBirthday()));
     }
 
     /**
@@ -47,6 +100,8 @@ public interface ReadOnlyPerson {
                 .append(getEmail())
                 .append(" Address: ")
                 .append(getAddress())
+                .append(" Birthday: ")
+                .append(getBirthday())
                 .append(" Tags: ");
         getTags().forEach(builder::append);
         return builder.toString();
